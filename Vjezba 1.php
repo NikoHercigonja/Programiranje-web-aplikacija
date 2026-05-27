0eae203034346e14e6143a8b417f0babe86d5fa8
NikoHercigonja <niko20.herc@gmail.com>
2026-05-27 19:36:46 +0200

    Create Vjezba 1.php

---
+++ Vjezba 1.php
@@ -0,0 +1,10 @@
+<?php
+$naslov = "Moj prvi PHP dokument";
+$autor = "Niko Hercigonja";
+
+echo "<h1>$naslov</h1>";
+echo "<p>Ovu stranicu izradio je $autor.</p>";
+echo '<a href="https://www.index.hr" target="_blank">Posjeti INDEX.HR</a>';
+?>
+
+<!-- Naziv datoteke: vjezba1a.php -->
